c3b78a7502799de3732f701c5a0e468ff112c422
wjddn0218 <79313880+wjddn0218@users.noreply.github.com>
2021-04-30 20:26:44 +0900

    first

---
+++ risk-of-drug/read.py
@@ -0,0 +1,3 @@
+x*y=2
+x=y
+print(x)
---
+++ root2/root2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  root2
+//
+//  Created by 정현우 on 2021/04/05.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
